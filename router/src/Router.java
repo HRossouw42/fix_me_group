98e9f9254ec216378f7b5d3804ca89fdfeb0970b
Harmun <hrossouw@student.wethinkcode.co.za>
2019-10-24 16:54:15 +0200

    pre hardcoded ports

--- router/src/Router.java
+++ router/src/Router.java
@@ -10,51 +10,6 @@ import java.net.UnknownHostException;
 // official Java multithreading tutorial -> https://docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html
 public class Router {
 
-    /*
-    public static void main(String[] args) throws IOException {
-
-//        if (args.length != 1) {
-//            System.err.println("Usage: java KnockKnockServer <port number>");
-//            System.exit(1);
-//        }
-
-        //int portNumber = Integer.parseInt(args[0]);
-        int portNumber = 8000;
-        boolean listening = true;
-
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber); // create socket object that listens on portNumber
-                Socket clientSocket = serverSocket.accept(); // accept connection from client
-
-                // get socket's input/output stream and opens readers and writers on them
-                PrintWriter out =
-                        new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-
-            String inputLine, outputLine;
-
-            // Initiate conversation with client
-            RouterProtocol kkp = new RouterProtocol(); // protocol object keeps track of states
-            outputLine = kkp.processInput(null); //runs protocol's process input function
-            out.println(outputLine);
-
-            //as long as client/server are connected, the server reads/writes to socket
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = kkp.processInput(inputLine);
-                out.println(outputLine);
-                if (outputLine.equals("exit")) //ends when client sends this message
-                    break;
-            }
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
-        }
-    }
-
-     */
     public static void main(String[] args) throws IOException {
 
         if (args.length != 1) {
@@ -66,6 +21,7 @@ public class Router {
         int marketPortNumber = 5001;
         boolean listening = true;
 
+        System.out.println("Connect at least 1 Market and 1 Broker");
         try (
                 ServerSocket serverSocket = new ServerSocket(portNumber);
                 ServerSocket marketSocket = new ServerSocket(marketPortNumber);
