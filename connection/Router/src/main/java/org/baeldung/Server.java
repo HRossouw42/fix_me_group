5e2b61fd0d06a73e9ae62385bcb09f240c084abb
PandaGears <seraphmite1@gmail.com>
2019-10-24 13:50:53 +0200

    MULTI MODULE MAVEEEEEN

---
+++ connection/Router/src/main/java/org/baeldung/Server.java
@@ -0,0 +1,25 @@
+package org.baeldung;
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+
+class Server {
+    public static void main(String args[]){
+        String data = "TACO";
+        try{
+            ServerSocket serve = new ServerSocket(5000);
+            Socket sock = serve.accept();
+            System.out.print("I'm in: " + serve.getLocalPort() +"\n");
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            System.out.print("Sending string: " + data + "\n");
+            out.print(data);
+            out.close();
+            sock.close();
+            serve.close();
+        }
+        catch(Exception e){
+            System.out.print("NOPE");
+        }
+    }
+}
