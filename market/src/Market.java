f587a7efc2e4cfbd814c7703151172d797e91207
Harmun <hrossouw@student.wethinkcode.co.za>
2019-11-05 13:25:21 +0200

    market fixed now router time

--- market/src/Market.java
+++ market/src/Market.java
@@ -10,7 +10,73 @@ public class Market {
         String hostName = "localhost";
         int portNumber = 5001;
 
+        //adding instruments
+        InstrumentList joe = new InstrumentList(150, "Joe");
+        System.out.println("employeeNumber before= "
+                + joe .getEmployeeNumber());
+        System.out.println("employeeName before= "
+                + joe .getEmployeeName());
+
+
         System.out.println("Attempting connection...");
+
+        try (
+                Socket kkSocket = new Socket(hostName, portNumber); //open a socket that is connected to the server running on the specific port number
+        ) {
+            ObjectOutputStream clientOutputStream = new
+                    ObjectOutputStream(kkSocket.getOutputStream());
+            ObjectInputStream clientInputStream = new
+                    ObjectInputStream(kkSocket.getInputStream());
+
+            clientOutputStream.writeObject(joe);
+
+            joe= (InstrumentList) clientInputStream.readObject();
+
+            System.out.println("employeeNumber after= "
+                    + joe .getEmployeeNumber());
+            System.out.println("employeeName after= "
+                    + joe .getEmployeeName());
+
+            clientOutputStream.close();
+            clientInputStream.close();
+
+            /*
+            BufferedReader stdIn =
+                    new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            //implement communication between client and server
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer); //server speaks first, so client must listen
+                if (fromServer.equals("exit")) //if server says this, end
+                    break;
+
+                //to get id
+                if (id == 0) {
+                    String[] getID = fromServer.split(" ");
+                    id = Integer.parseInt(getID[1]);
+                }
+
+                fromUser = stdIn.readLine(); //take input
+                if (fromUser != null) {
+                    System.out.println(id + ": " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+
+             */
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        /*
         try (
 
                 Socket kkSocket = new Socket(hostName, portNumber); //open a socket that is connected to the server running on the specific port number
@@ -49,13 +115,17 @@ public class Market {
                     hostName);
             System.exit(1);
         }
+
+         */
     }
 
-//    public static void main(String[] args) throws IOException {
-//
-//        Market market = new Market();
-//        market.run();
-//    }
+    public static void main(String[] args) throws IOException {
+
+        Market market = new Market();
+        market.run();
+    }
+
+    /*
 public static void main(String[] arg) {
     try {
         InstrumentList joe = new InstrumentList(150, "Joe");
@@ -87,5 +157,7 @@ public static void main(String[] arg) {
 
     } catch (Exception e) {System.out.println(e); }
 }
+
+     */
 
 }
