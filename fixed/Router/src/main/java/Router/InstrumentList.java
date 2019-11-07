2d25e84293624fc2d9a9aff3862ce73feaeb600f
jadinek <jkruger@student.wethinkcode.co.za>
2019-11-07 14:41:45 +0200

    copy of IL in Router

---
+++ fixed/Router/src/main/java/Router/InstrumentList.java
@@ -0,0 +1,43 @@
+package Market;
+
+import java.io.Serializable;
+
+// https://www.developer.com/design/article.php/3597071/Serializing-an-Object-via-a-ClientServer-Connection.html
+public class InstrumentList implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private int potionNumber;
+    private String potionName;
+
+    InstrumentList(int num, String name) {
+        potionNumber = num;
+        potionName = name;
+    }
+
+    public int getPotionNumber() {
+        return potionNumber;
+    }
+
+    public void setPotionNumber(int num) {
+        potionNumber = num;
+    }
+
+    public void addPotionNumber() {
+        potionNumber++;
+    }
+
+    public void reducePotionNumber() {
+        potionNumber--;
+    }
+
+    public String getPotionName() {
+        return potionName;
+    }
+
+    public void setPotionName(String name) {
+        potionName = name;
+    }
+}
